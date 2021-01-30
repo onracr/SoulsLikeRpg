d97cc7126fc9fc840e79882976dd6c17a1bbfdd5
Onur Acar <onracr_ms87@hotmail.com>
2021-01-30 19:10:05 +0300

    Pickup System is implemented.

--- Source/SoulsLikeRpg/SoulsLikeRpgGameMode.cpp
+++ Source/SoulsLikeRpg/SoulsLikeRpgGameMode.cpp
@@ -7,7 +7,7 @@
 ASoulsLikeRpgGameMode::ASoulsLikeRpgGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/_Main/Player/BP_HeroCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
